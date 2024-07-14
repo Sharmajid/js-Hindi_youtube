fdda54acf07ddaa1a2309b9a01fe2630ceb7396b
Anand Sharma <154674308+Sharmajid@users.noreply.github.com>
2024-07-14 10:41:47 +0000

    convrsion of datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=144553
+let accountEmail="Anand@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountstate ;
+// accountId=2// Not allowed 
+
+accountEmail="hfd@gmail.com"
+accountPassword="1334454"
+accountCity="Bhopal"// this is not a proper use in all time 
+
+
+console.log(accountId);
+/*
+prefer not to be use var 
+because of issue in block scope and functional scope 
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
